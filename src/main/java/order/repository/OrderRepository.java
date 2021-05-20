13de12909922718e994e623a9bfd92cde8d0c6d3
nsree <nsdree.nsik@gmil.com>
2021-05-20 10:19:46 +0530

    project

--- src/main/java/order/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package order.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import order.entity.Order;
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-	public Order findByOrderId(Integer orderId);
-}
